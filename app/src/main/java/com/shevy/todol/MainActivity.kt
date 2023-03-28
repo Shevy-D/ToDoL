effd402a1e7eab5d200189590cba76f25109895a
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2023-03-28 22:07:39 +0700

    Added DataBaseHelper

--- app/src/main/java/com/shevy/todol/MainActivity.kt
+++ app/src/main/java/com/shevy/todol/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.shevy.todol
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.shevy.todol.utils.DataBaseHelper
 
 class MainActivity : AppCompatActivity() {
+
+    private var myDB: DataBaseHelper? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mRecyclerview: RecyclerView = findViewById(R.id.recyclerViewMA)
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        myDB = DataBaseHelper(this@MainActivity)
+
+        fab.setOnClickListener {
+            Toast.makeText(this, "You clicked me", Toast.LENGTH_SHORT).show()
+            //AddNewTask.newInstance().show(supportFragmentManager, AddNewTask.TAG)
+        }
     }
 }
---
+++ app/src/main/java/com/shevy/todol/model/ToDoModel.kt
@@ -0,0 +1,5 @@
+package com.shevy.todol.model
+
+data class ToDoModel(var task: String? = null, var id: Int = 0, var status: Int = 0) {
+
+}
